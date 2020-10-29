29c8da4be0a2484361a8317699e63f563f93c860
Humberto Giuri <humbertogiuri@gmail.com>
2020-10-29 20:15:51 -0300

    test

--- utils.hs
+++ utils.hs
@@ -1,6 +1,7 @@
 import System.IO ()
 import Data.List ()
 import Data.Char ()
+import System.Random (randomRs, mkStdGen)
 
 --transforma umas string para double
 toDouble :: String -> Double
@@ -28,12 +29,32 @@ distanciaEuclediana [] _ = error "ponto invalido"
 distanciaEuclediana _ [] = error "ponto invalido"
 distanciaEuclediana xs ys = sqrt . sum $ zipWith (\x y -> (x - y)^2) xs ys 
 
-{-
+tem :: Eq a => a -> [a] -> Bool
+tem _ [] = False
+tem y (x:xs)
+   | x == y = True
+   | otherwise = tem y xs
+
+
+removeDup :: Eq a => [a] -> [a]
+removeDup l = removeD l []
+   where
+     removeD [] _ = []
+     removeD (x:xs) ls
+        | tem x ls = removeD xs ls
+        | otherwise = x: removeD xs (x:ls)
+
+geraVetorValoresAleatorios :: Int -> [Int]
+geraVetorValoresAleatorios seed = take 50 (removeDup ((randomRs (1,150) (mkStdGen seed) :: [Int])))
+
+separaPorInd :: [Int] -> [a] -> ([a], [a])
+separaPorInd lind base = (base1, base2)
+   where
+      base1 = [base!!i|i<-[0..(length(base)-1)], 
+         not (elem i lind)] 
+      base2 = [base!!i|i <- lind]
+
 main :: IO ()
 main = do
-    content <- readFile "iris.csv"
-    let linhas = map split $ lines content
-    let linhasFormatadas = map formataLinha linhas
-    mapM_ print  linhasFormatadas    
-    return()
--}    
+   content <- readFile "iris.csv"
+   print . lines $ content
